0f8ff50367d729c477991234c56322c47ef2775f
sarang <sarangravikumar1999@gmail.com>
2025-01-30 13:00:57 +0530

    Hero Content

--- app/(pages)/Hero.tsx
+++ app/(pages)/Hero.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import Image from 'next/image';
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 
 const Hero = () => {
@@ -16,13 +18,12 @@ const Hero = () => {
 
             <div className="grid lg:mt-28 mt-5 grid-cols-1 lg:grid-cols-2 lg:gap-28">
                 {/* Right content - Image */}
-                <div className="relative flex justify-center align-items-center mt-8 lg:mt-0 order-1 lg:order-2">
+                <div className="relative flex justify-center align-items-center mt-8 lg:mt-0 order-1 lg:order-2 max-w-[600px] h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh]">
                     <Image
                         src="/background.png"
                         alt="Image"
                         width={500}
                         height={600}
-                        // className="mx-auto object-cover lg:mx-0"
                     />
                 </div>
 
@@ -32,20 +33,16 @@ const Hero = () => {
                     <p className="text-lg text-gray-600 dark:text-gray-400">Hey Buddy, Myself</p>
                     <h1 className="mt-2 text-4xl font-bold text-gray-900 sm:text-5xl lg:text-6xl">Sarang Ravikumar</h1>
                     <p className="mt-4 text-lg text-gray-600">Full Stack Developer</p>
-                    {/* <div className="mt-8 flex gap-4"
+                    <div className="mt-8 flex gap-4">
                         <Link href="#" className="rounded-full bg-gray-900 p-2 text-white hover:bg-gray-700">
-                            <Globe className="h-5 w-5" />
-                            <span className="sr-only">Website</span>
-                        </Link>
-                        <Link href="#" className="rounded-full bg-gray-900 p-2 text-white hover:bg-gray-700">
-                            <Github className="h-5 w-5" />
+                            <FaGithub />
                             <span className="sr-only">GitHub</span>
                         </Link>
                         <Link href="#" className="rounded-full bg-gray-900 p-2 text-white hover:bg-gray-700">
-                            <Linkedin className="h-5 w-5" />
+                            <FaLinkedin />
                             <span className="sr-only">LinkedIn</span>
                         </Link>
-                    </div> */}
+                    </div>
                 </div>
 
             </div>
